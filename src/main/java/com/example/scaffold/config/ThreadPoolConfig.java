f884ba560db0f4d0440c855168e01cb771699c8a
jluncc <cjljinglun@163.com>
2020-08-29 12:34:44 +0800

    feat: 1.配置文件修改，线程池配置放在配置文件；2.引入actuator

--- src/main/java/com/example/scaffold/config/ThreadPoolConfig.java
+++ src/main/java/com/example/scaffold/config/ThreadPoolConfig.java
@@ -1,5 +1,6 @@
 package com.example.scaffold.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -13,35 +14,35 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @Configuration
 public class ThreadPoolConfig {
-    /**
-     * 核心线程数（默认线程数）
-     */
-    private static final int corePoolSize = 10;
-    /**
-     * 最大线程数
-     */
-    private static final int maxPoolSize = 20;
-    /**
-     * 允许线程空闲时间（单位：默认为秒）
-     */
-    private static final int keepAliveTime = 10;
-    /**
-     * 缓冲队列大小
-     */
-    private static final int queueCapacity = 50;
-    /**
-     * 线程池名前缀
-     */
-    private static final String threadNamePrefix = "async-thread-";
+
+    // 核心线程数（默认线程数）
+    @Value("${common.thread-pool.core-pool-size}")
+    private int CORE_POOL_SIZE;
+
+    // 最大线程数
+    @Value("${common.thread-pool.max-pool-size}")
+    private int MAX_POOL_SIZE;
+
+    // 允许线程空闲时间（单位：默认为秒）
+    @Value("${common.thread-pool.keep-alive-time}")
+    private int KEEP_ALIVE_TIME;
+
+    // 缓冲队列大小
+    @Value("${common.thread-pool.queue-capacity}")
+    private int QUEUE_CAPACITY;
+
+    // 线程池名前缀
+    @Value("${common.thread-pool.thread-name-prefix}")
+    private String THREAD_NAME_PREFIX = "async-thread-";
 
     @Bean("myTaskExecutor") // bean的名称，默认为首字母小写的方法名
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaxPoolSize(maxPoolSize);
-        executor.setQueueCapacity(queueCapacity);
-        executor.setKeepAliveSeconds(keepAliveTime);
-        executor.setThreadNamePrefix(threadNamePrefix);
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
 
         // 线程池对拒绝任务的处理策略
         // CallerRunsPolicy：由调用线程（提交任务的线程）处理该任务
